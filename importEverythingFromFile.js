64323d665a990b2ca73beedd26b7b95d86bd0a36
Joana <joanajteixeira@outlook.com>
2019-02-13 13:28:11 +0000

    //ES6: Use * to Import Everything from a File

---
+++ importEverythingFromFile.js
@@ -0,0 +1,7 @@
+//ES6: Use * to Import Everything from a File
+
+/*The code below requires the contents of a file, "capitalize_strings", found in the same directory as it, imported. Add the appropriate import * statement to the top of the file, using the object provided.*/
+
+import * as myImports from "capitalize_strings";
+
+"use strict";
