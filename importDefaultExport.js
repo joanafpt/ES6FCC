40358cd7fb407282cdf90805b6914e4109f4be5e
Joana <joanajteixeira@outlook.com>
2019-02-13 13:28:05 +0000

    //ES6: Import a Default Export

---
+++ importDefaultExport.js
@@ -0,0 +1,6 @@
+//ES6: Import a Default Export
+//In the following code, please import the default export, subtract, from the file "math_functions", found in the same directory as this file.
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
